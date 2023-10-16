a3a672795e0ac6ef4f04f44fb6453f15ea7389c6
Jamie Hamed <jamiehamed29@gmail.com>
2023-10-16 18:20:11 -0400

    Made an amogus and added wasd and space inputs

--- python/pygame/gamepy.py
+++ python/pygame/gamepy.py
@@ -1,23 +1,34 @@
 import pygame 
 import sys
-import time
-pygame.init()
 
-screen = pygame.display.set_mode([500, 500])
-screen.fill((255, 255, 255)) 
+pygame.init() 
+ 
+canvas = pygame.display.set_mode((500, 500))
+canvas.fill((200, 0,0 ))
 
-while True:
-    for event in pygame.event.get():
-        #screen.fill((255, 255, 255)) 
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            print("hi")    
-            pygame.draw.circle(screen, (100, 0, 200), (250, 250), 100)
-            time.sleep(10)
-            screen.fill((255, 255, 255))
+pygame.display.set_caption("This Pygame is a Pyshame") 
+exit = False
+
+pygame.draw.rect(canvas, (0, 0, 255), [50, 175, 400, 100], 0)
+pygame.draw.rect(canvas, (255, 255, 255), [50, 175, 100, 50], 0)
+pygame.display.update() 
+while not exit: 
     
-    
-    pygame.display.flip()
-                   
+ for event in pygame.event.get():
+     if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
+     if event.type == pygame.KEYDOWN:
+         if event.key == pygame.K_SPACE:
+             print("space")
+         elif event.key == pygame.K_w:
+             print("w")
+         elif event.key == pygame.K_a:
+             print("a")
+         elif event.key == pygame.K_s:
+             print("s")
+         elif event.key == pygame.K_d:
+             print("d")
+ 
+ pygame.display.update() 
+
