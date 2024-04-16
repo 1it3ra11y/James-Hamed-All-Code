e217a01031a08f381f633bd56091db558f7b2d3d
Jamie Hamed <jamiehamed29@gmail.com>
2024-04-15 20:45:49 -0400

    added player movement

--- python/pygame/gamepy.py
+++ python/pygame/gamepy.py
@@ -1,34 +1,31 @@
-import pygame 
-import sys
-
+import pygame, sys
 pygame.init() 
- 
-canvas = pygame.display.set_mode((500, 500))
-canvas.fill((200, 0,0 ))
 
-pygame.display.set_caption("This Pygame is a Pyshame") 
-exit = False
+screen = pygame.display.set_mode((500, 500))
+clock = pygame.time.Clock()
+Testsurface = pygame.Surface((100, 100))
+Testsurface.fill((0, 0, 255))
+player = Testsurface.get_rect(center = (250, 250))
 
-pygame.draw.rect(canvas, (0, 0, 255), [50, 175, 400, 100], 0)
-pygame.draw.rect(canvas, (255, 255, 255), [50, 175, 100, 50], 0)
-pygame.display.update() 
-while not exit: 
+while True:
+    screen.fill((0, 0, 0))
+    screen.blit(Testsurface, player)
     
- for event in pygame.event.get():
-     if event.type == pygame.QUIT:
-        pygame.quit()
-        sys.exit()
-     if event.type == pygame.KEYDOWN:
-         if event.key == pygame.K_SPACE:
-             print("space")
-         elif event.key == pygame.K_w:
-             print("w")
-         elif event.key == pygame.K_a:
-             print("a")
-         elif event.key == pygame.K_s:
-             print("s")
-         elif event.key == pygame.K_d:
-             print("d")
- 
- pygame.display.update() 
-
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] == True:
+        player.move_ip(-2, 0)
+    elif key[pygame.K_d] == True:
+        player.move_ip(2, 0)
+    elif key[pygame.K_s] == True:
+        player.move_ip(0, 2)
+    elif key[pygame.K_w] == True:
+        player.move_ip(0, -2)
+        
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()    
+              
+    pygame.display.update()
+    clock.tick(60)
+    
